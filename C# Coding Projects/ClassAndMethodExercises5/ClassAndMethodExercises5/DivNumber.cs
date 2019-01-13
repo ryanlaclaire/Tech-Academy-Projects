7993193c40f232e9dcacc1d409d2a882f7a83ec4
Ryan LaClaire <43187889+ryanlaclaire@users.noreply.github.com>
2019-01-13 10:54:58 -0500

    Add files via upload

---
+++ C# Coding Projects/ClassAndMethodExercises5/ClassAndMethodExercises5/DivNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodExercises5
+{
+    public class DivNumber
+    {
+        public void div2(int num1)
+        {
+            int answer = num1 / 2;
+            Console.WriteLine($"\n{num1} / 2 = {answer}");
+        }
+        public void Output(out string input1)
+        {
+            input1 = "\nIt's nice to receive some good input."; ;
+            Console.WriteLine("\nIsn't this just lovely?");
+        }
+        public void div2(string input2)
+        {
+            Console.WriteLine(input2);
+        }
+    }
+    static class Step6
+    {
+        //  This won't do anything outside of this area.
+    }
+}
---
+++ C# Coding Projects/ClassAndMethodExercises5/ClassAndMethodExercises5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodExercises5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DivNumber divNum = new DivNumber();
+            Console.WriteLine("Input a number and we will divide it by 2.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            divNum.div2(num1);
+            divNum.Output(out string input2);
+            divNum.div2(input2);
+            Console.ReadLine();
+
+        }
+    }
+}
