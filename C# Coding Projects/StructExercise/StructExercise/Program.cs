624844f0b710f34a8b4b1dddc780cd4b2832f9f6
Ryan LaClaire <43187889+ryanlaclaire@users.noreply.github.com>
2019-01-13 11:01:37 -0500

    Add files via upload

---
+++ C# Coding Projects/StructExercise/StructExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number getNum = new Number();
+            getNum.Amount = 34.56m;
+            Console.WriteLine(getNum.Amount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C# Coding Projects/TextWritingExercise/TextWritingExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TextWritingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number.");
+            string num = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\ryanl\Logs\user.txt", num);
+            string text = File.ReadAllText(@"C:\Users\ryanl\Logs\user.txt");
+            Console.WriteLine($"The contents of the file user.txt is: {text}");
+            Console.ReadLine();
+        }
+    }
+}
