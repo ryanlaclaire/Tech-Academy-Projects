fe0e840e8ded8bc925b35fd43877013952dfb139
Ryan LaClaire <43187889+ryanlaclaire@users.noreply.github.com>
2019-01-13 08:09:02 -0500

    Add files via upload

---
+++ C# Coding Projects/ContosoUniversity/ContosoUniversity/ViewModels/AssignedCourseData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ContosoUniversity.ViewModels;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class AssignedCourseData
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
