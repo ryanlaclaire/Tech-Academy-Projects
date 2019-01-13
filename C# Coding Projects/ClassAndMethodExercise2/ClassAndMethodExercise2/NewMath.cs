ac2d2c9d3a61acc26a80657362a42bd647267854
Ryan LaClaire <43187889+ryanlaclaire@users.noreply.github.com>
2019-01-13 10:10:21 -0500

    Add files via upload

---
+++ C# Coding Projects/ClassAndMethodExercise2/ClassAndMethodExercise2/NewMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodExercise2
+{
+    public class NewMath
+    {
+        public static void Integers(int num1, int num2)
+        {
+            int change = num1 * 5;
+            Console.WriteLine($"{num1} times 5 = {change}");
+            Console.WriteLine($"The second number input is: {num2}");
+        }
+    }
+}
---
+++ C# Coding Projects/ClassAndMethodExercise2/ClassAndMethodExercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewMath newMath = new NewMath();
+            NewMath.Integers(3, 9);
+            NewMath.Integers(num1: 5, num2: 11);
+
+            Console.ReadLine();
+            
+
+            //newMath = Integers(3, 9);      //This line won't find Integers. Visual Studio says it doesn't exist.
+            
+        }
+    }
+}
