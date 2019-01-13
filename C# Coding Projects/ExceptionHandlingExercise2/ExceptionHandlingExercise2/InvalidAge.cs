36b859f1f94197aa9010dbc4f1b389a583b293df
Ryan LaClaire <43187889+ryanlaclaire@users.noreply.github.com>
2019-01-13 10:58:00 -0500

    Add files via upload

---
+++ C# Coding Projects/ExceptionHandlingExercise2/ExceptionHandlingExercise2/InvalidAge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingExercise2
+{
+    public class InvalidAge : Exception
+    {
+        public InvalidAge()
+            : base()
+        {
+
+        }
+        public InvalidAge(String message)
+            : base(message)
+        {
+
+        }
+
+    }
+}
---
+++ C# Coding Projects/ExceptionHandlingExercise2/ExceptionHandlingExercise2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                bool validAge = false;
+                int age = 1;
+                while (!validAge)
+                {
+                    Console.WriteLine("Please enter your age.");
+                    validAge = int.TryParse(Console.ReadLine(), out age);
+
+                    if (!validAge) { Console.WriteLine("Please enter a whole number only with no decimals."); }
+                }
+
+                if (age <= 0)
+                {
+                    throw new InvalidAge();
+
+                }
+                DateTime yearsOld = DateTime.Today.AddYears(-age);
+                Console.WriteLine($"\nYou were born in {yearsOld.Year}.");
+            }
+            catch (InvalidAge)
+            {
+                Console.WriteLine("Error. You must enter a number greater than zero.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error. That is not valid input.");
+            }
+                Console.ReadLine();
+            
+        }
+    }
+}
