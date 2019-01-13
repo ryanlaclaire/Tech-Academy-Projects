8331727544d11ac27c94807107770e122e77c2ad
Ryan LaClaire <43187889+ryanlaclaire@users.noreply.github.com>
2019-01-13 11:00:43 -0500

    Add files via upload

---
+++ C# Coding Projects/OperatorOverloadExercise/OperatorOverloadExercise/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadExercise
+{
+    public class Employee
+    {
+
+        public int ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public static bool operator== (Employee emp1, Employee emp2)
+        {
+            return emp1.ID == emp2.ID;
+        }
+  
+        public static bool operator!= (Employee emp1, Employee emp2)
+        {
+            
+            return emp1.ID != emp2.ID;
+        }
+        public override bool Equals(object obj)
+        {
+            Employee other = (Employee)obj;
+            return other.ID == this.ID;
+        }
+       
+    }
+}
---
+++ C# Coding Projects/OperatorOverloadExercise/OperatorOverloadExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee empTest = new Employee() { ID = 001};
+            Employee empTest2 = new Employee() { ID = 002 };
+            List<Employee> employee = new List<Employee>();
+
+            employee.Add(new Employee() { ID = 001, firstName = "John", lastName = "Doe" });
+            employee.Add(new Employee() { ID = 002, firstName = "Abbie", lastName = "LaClaire" });
+            employee.Add(new Employee() { ID = 003, firstName = "Ryan", lastName = "LaClaire" });
+            employee.Add(new Employee() { ID = 004, firstName = "Jesse", lastName = "James" });
+
+            bool result = empTest == empTest2;
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
