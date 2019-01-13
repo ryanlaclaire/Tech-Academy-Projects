ab258f49b772ddbbc755e48022ad3d1a227b6bca
Ryan LaClaire <43187889+ryanlaclaire@users.noreply.github.com>
2019-01-13 10:55:58 -0500

    Add files via upload

---
+++ C# Coding Projects/DateTimeExercise/DateTimeExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{DateTime.Now}\n");
+            Console.WriteLine("Please input a whole number.");
+            int getNum = Convert.ToInt32(Console.ReadLine());
+            DateTime futureTime = DateTime.Now.AddHours(getNum);
+            Console.WriteLine($"\nThe time in {getNum} hours will be {futureTime}.");
+            Console.ReadLine();
+
+        }
+        
+    }
+}
