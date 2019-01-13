14209e7548fca0d7737f5b4d0f56a2abc0110a05
Ryan LaClaire <43187889+ryanlaclaire@users.noreply.github.com>
2019-01-13 08:36:40 -0500

    Add files via upload

---
+++ C# Coding Projects/AcmeInsuranceQuotes/AcmeInsuranceQuotes/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AcmeInsuranceQuotes
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
