bb75d477a4fa38c3279cd5432ec5a217a26853fa
Ryan LaClaire <43187889+ryanlaclaire@users.noreply.github.com>
2019-01-13 10:55:31 -0500

    Add files via upload

---
+++ C# Coding Projects/ConstructorChainExercise/ConstructorChainExercise/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChainExercise
+{
+    public class Employee
+    {
+        public Employee(int EmpId): this("", EmpId)
+        {
+
+        }
+
+        public Employee(string name, int EmpId)
+        {
+            Record = new List<Employee>();
+            ID = EmpId;
+            Name = name;
+        }
+
+        private List<Employee> _record = new List<Employee>();
+        public List<Employee> Record { get { return _record; } set { _record = value; } }        // we would also modify to List<T> so we could change the data type of the list
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Guid newId { get; set; }
+    }
+}
---
+++ C# Coding Projects/ConstructorChainExercise/ConstructorChainExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChainExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string compName = "Acme Manufacturing";
+
+            var employee = new Employee(001);
+            employee.Name = "James Smith";
+
+            Console.WriteLine($"Welcome to {compName}, {employee.Name}!");
+            Console.ReadLine();
+
+        }
+    }
+}
