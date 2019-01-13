b20e867ae0ffdc4fa9dad92478cad3dbfa4b8772
Ryan LaClaire <43187889+ryanlaclaire@users.noreply.github.com>
2019-01-13 10:11:13 -0500

    Add files via upload

---
+++ C# Coding Projects/C-SharpExercises/C-SharpExercises/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SharpExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("The Tech Academy");
+            //Console.WriteLine("Student Daily Report");
+            //Console.WriteLine("What course are you on?");
+            //string currentCourse = Console.ReadLine();
+            //Console.WriteLine("What page number?");
+            //string currentPage = Console.ReadLine();
+            //Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            //string getAnswer = Console.ReadLine();
+            //bool needHelp = Convert.ToBoolean(getAnswer);
+            //Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            //string goodExperience = Console.ReadLine();
+            //Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            //string goodFeedback = Console.ReadLine();
+            //Console.WriteLine("How many hours did you study today?");
+            //int numberOfHours = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            //Console.ReadLine();
+
+
+            int num1 = 10;
+            int num2 = 3;
+
+            int remainder = num1 % num2;
+
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+        }
+    }
+}
