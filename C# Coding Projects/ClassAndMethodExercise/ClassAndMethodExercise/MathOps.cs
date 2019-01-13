cada9b92216e780eca5315b1bd35fc002533f860
Ryan LaClaire <43187889+ryanlaclaire@users.noreply.github.com>
2019-01-13 10:09:41 -0500

    Add files via upload

---
+++ C# Coding Projects/ClassAndMethodExercise/ClassAndMethodExercise/MathOps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodExercise
+{
+    public class MathOps
+    {
+        public static void  AddNum(int userNum)       
+        {
+            int addAnswer = userNum + 5;
+            Console.WriteLine($"{userNum} + 5 equals: {addAnswer}");
+        }
+
+        public static void MultNum(int userNum)
+        {
+            int multAnswer = userNum * 3;
+            Console.WriteLine($"{userNum} * 3 equals: {multAnswer}");
+        }
+
+        public static void DivNum(int userNum)
+        {
+            int divAnswer = userNum / 2;
+            Console.WriteLine($"{userNum} / 2 equals: {divAnswer}");
+        }
+    }
+
+}
---
+++ C# Coding Projects/ClassAndMethodExercise/ClassAndMethodExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a whole number.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            MathOps.AddNum(userNum);
+            MathOps.MultNum(userNum);
+            MathOps.DivNum(userNum);
+            Console.ReadLine();
+        }
+    }
+}
