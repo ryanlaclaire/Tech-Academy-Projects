d61422e760d6695531a0de8d82f89f033158c8a9
Ryan LaClaire <43187889+ryanlaclaire@users.noreply.github.com>
2019-01-13 08:06:47 -0500

    Add files via upload

---
+++ C# Coding Projects/ContosoUniversity/ContosoUniversity/DAL/SchoolConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+using ContosoUniversity.DAL;
+using System.Data.Entity.Infrastructure.Interception;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolConfiguration : DbConfiguration
+    {
+        public SchoolConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+            DbInterception.Add(new SchoolInterceptorTransientErrors());
+            DbInterception.Add(new SchoolInterceptorLogging());
+        }
+    }
+}
