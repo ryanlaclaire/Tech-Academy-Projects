ac6b12428096a20af15bca12ed12edcc0601083f
Ryan LaClaire <43187889+ryanlaclaire@users.noreply.github.com>
2019-01-13 10:54:26 -0500

    Add files via upload

---
+++ C# Coding Projects/ClassAndMethodExercise4/ClassAndMethodExercise4/Optional.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodExercise4
+{
+    public class Optional
+    {
+        public static void TwoNums(int num1, int num2 = 0)
+        {
+            if (num1 > 0 && num2 == 0)
+            {
+                int answer1 = num1 * 5;
+                Console.WriteLine($"You only entered 1 number. {num1} * 5 = {answer1}");
+            }
+            else if (num1 > 0 && num2 > 0)
+            {
+                int answer2 = num1 * num2;
+                Console.WriteLine($"You entered 2 numbers. {num1} * {num2} = {answer2}");
+            }
+            else
+            {
+                Console.WriteLine("Well that didn't work at all.");
+            }
+        }
+    }
+}
---
+++ C# Coding Projects/ClassAndMethodExercise4/ClassAndMethodExercise4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodExercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Optional option = new Optional();
+            Console.WriteLine("You will be asked to input two numbers. " +
+                "\nThe first number is necessary. The second number is optional.\n");
+            Console.WriteLine("Please input the first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("(Optional) Please input the second number:");
+            string input = Console.ReadLine();
+            int num2 = 0;
+            if (!int.TryParse(input, out num2))
+            {
+                Optional.TwoNums(num1);
+            }
+            else
+            {
+                Optional.TwoNums(num1, num2);
+            }
+       
+            Console.ReadLine();
+        }
+    }
+}
